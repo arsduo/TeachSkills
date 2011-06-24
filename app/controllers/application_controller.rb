04a56a3583e2abc86d8cad7d6ccbcd9e2c3d1c01
Alex Koppel <alex+git@alexkoppel.com>
2011-06-25 01:00:55 +0200

    Added Facebook login button.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  before_filter :parse_facebook_cookies
+  
+  private
+  
+  def parse_facebook_cookies
+    oauth = Koala::Facebook::OAuth.new
+    Rails.logger.debug("We have an OAuth object for #{oauth.app_id}")
+    if fb_info = oauth.get_user_info_from_cookie(cookies)
+      Rails.logger.debug("Facebook user: #{fb_info.inspect}")
+    end
+  end
 end
