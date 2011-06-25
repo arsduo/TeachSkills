8e0714d31968dbf0ec12defd5990a2c56d35f78d
Michael Gajda <michael@apecore.de>
2011-06-25 05:31:36 +0200

    made a list of newest classes at index page

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,12 @@ Mongoid.master.collections.reject { |c| c.name =~ /^system/}.each(&:drop)
 puts 'SETTING UP DEFAULT USER LOGIN'
 user = User.create! :name => 'Micha', :email => 'm@g.de', :password => 'please', :password_confirmation => 'please'
 puts 'New user created: ' << user.name
+puts 'SETTING UP CLASSROOMS'
+classroom = Classroom.create! :name => 'Micha macht den Guru', :summary => 'Alles und die Welt'
+puts 'New Classroom created: ' << classroom.name
+classroom = Classroom.create! :name => 'Micha macht den Koch', :summary => 'Essen geht immer!'
+puts 'New Classroom created: ' << classroom.name
+classroom = Classroom.create! :name => 'Micha macht den Coder', :summary => '1337h4xx0r'
+puts 'New Classroom created: ' << classroom.name
+classroom = Classroom.create! :name => 'Micha macht den Rockstar', :summary => 'It is time to kick ass!'
+puts 'New Classroom created: ' << classroom.name
